cb3d442692864b73982cd139e4ea3cdc09f479f5
T Riboulet <riboulet@gmail.com>
2010-02-21 11:10:47 +0100

    moving delete link to button to
    in order to have those working with current browsers

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,23 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+/*var MyJsObject = {
+  
+  linkToDelete: function(event) {
+    event.stop();
+    var e = event.element();
+    var a = e.readAttribute('href') ? e : e.up('a');
+    var form = FORM({action:a.href, method:'post'},[
+      INPUT({type:'hidden',name:'_method',value:'delete'}),
+      INPUT({type:'hidden',name:'authenticity_token',value:authParams.get('authenticity_token')})]
+    );
+    form.submit();
+    return false;
+  }
+  
+};
+
+document.observe('dom:loaded', function(){
+  $$('a[data-method=delete]').each(function(a){ a.observe('click', MyJsObject.linkToDelete); });
+});*/
+
+
