705378305f624b3afcf3e8642c9adc546104fa3e
T Riboulet <riboulet@gmail.com>
2010-02-21 11:27:28 +0100

    adding attributes for users auth

--- db/migrate/20100221101739_create_users.rb
+++ db/migrate/20100221101739_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-
+      t.string :email
+      t.string :login
+      t.string :crypted_password
+      t.string :password_salt
+      t.string :persistence_token
       t.timestamps
     end
   end
