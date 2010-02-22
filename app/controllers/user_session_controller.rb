06ef587cc130c4e23aa5d8e778d76c315c5f5cb0
T Riboulet <riboulet@gmail.com>
2010-02-22 12:08:57 +0100

    adding user session controller code

--- app/controllers/user_session_controller.rb
+++ app/controllers/user_session_controller.rb
@@ -1,2 +1,26 @@
 class UserSessionController < ApplicationController
+  def new
+    @user_session = UserSession.new
+  end
+  
+  def create
+    @user_session = UserSession.new(params[:user_session])
+    if @user_session.save
+      flash[:notice] = t(:login_ok)
+			if active_order?
+				session[:active_order] = active_order.id
+			end
+	   	redirect_to root_url
+    else
+      redirect_to :action => 'new'
+    end
+  end
+  
+  def destroy
+    @user_session = UserSession.find
+		session[:active_order] = nil
+    @user_session.destroy
+    flash[:notice] = t(:logout_ok)
+    redirect_to root_url
+  end
 end
