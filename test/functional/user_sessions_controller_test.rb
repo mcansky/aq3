2a0e335eaf2f9ce885cf0b77378c829bf983d42f
T Riboulet <riboulet@gmail.com>
2010-02-22 14:14:02 +0100

    fixing typo

--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UserSessionControllerTest < ActionController::TestCase
+class UserSessionsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
