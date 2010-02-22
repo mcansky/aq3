8e4f0664d486152f39ad5e25266992a38dc7c8e5
T Riboulet <riboulet@gmail.com>
2010-02-22 17:24:35 +0100

    adding tests to limit keys display to owner

--- app/controllers/ssh_keys_controller.rb
+++ app/controllers/ssh_keys_controller.rb
@@ -2,11 +2,16 @@ class SshKeysController < ApplicationController
   before_filter :login_required
 
   def index
-    @keys = SshKey.all
+    @keys = current_user.ssh_keys
   end
   
   def show
     @key = SshKey.find(params[:id])
+    if @key.user != current_user
+      flash[:notice] = t(:sshkey_not_owner)
+      @key = nil
+      redirect_to root_url
+    end
   end
   
   def new
@@ -15,6 +20,7 @@ class SshKeysController < ApplicationController
   
   def create
     @key = SshKey.new(params[:ssh_key])
+    @key.user = current_user
     @key.extract_login
     if @key.save
       flash[:notice] = t(:sshkey_created_ok)
@@ -26,16 +32,25 @@ class SshKeysController < ApplicationController
   
   def edit
     @key = SshKey.find(params[:id])
+    if @key.user != current_user
+      flash[:notice] = t(:sshkey_not_owner)
+      @key = nil
+      redirect_to root_url
+    end
   end
   
   def update
     @key = SshKey.find(params[:id])
-    if @key.update_attributes(params[:ssh_key])
-      @key.extract_login
-      flash[:notice] = t(:sshkey_update_ok)
-      redirect_to @key
+    if @key.user == current_user
+      if @key.update_attributes(params[:ssh_key])
+        @key.extract_login
+        flash[:notice] = t(:sshkey_update_ok)
+        redirect_to @key
+      else
+        render :action => 'edit'
+      end
     else
-      render :action => 'edit'
+      flash[:notice] = t(:sshkey_not_owner)
     end
   end
   
