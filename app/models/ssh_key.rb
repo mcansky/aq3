96d33203ea1ed26b21255bd3af129aad1326f4e2
T Riboulet <riboulet@gmail.com>
2010-02-22 15:25:59 +0100

    adding relations to SshKey and User models

--- app/models/ssh_key.rb
+++ app/models/ssh_key.rb
@@ -1,2 +1,3 @@
 class SshKey < ActiveRecord::Base
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   acts_as_authentic
+  has_many :ssh_keys
+
   def self.find_by_login_or_email(login)
     find_by_login(login) || find_by_email(login)
   end
