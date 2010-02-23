c11efd9a77e02b52f7d7e613175f9c1ca1f47a71
T Riboulet <riboulet@gmail.com>
2010-02-23 12:24:02 +0100

    added local current_user method, fixed bad path gen
    in order to be able to access current_user in this controller added a method to do so
    fixed path generation

--- app/models/aq_repository.rb
+++ app/models/aq_repository.rb
@@ -3,47 +3,53 @@ require 'pathname'
 class AqRepository < ActiveRecord::Base
   before_save :repo_path
   after_save :repo_init
-  
+
   private
+  def current_user
+  	@current_user_session = UserSession.find
+  	@current_user = @current_user_session && @current_user_session.record
+  	return @current_user
+  end
+
   def repo_path
     # root dir is system home folder, need to exist prior to app launch
     # /home
     root_dir = Pathname(Settings.application.root_dir)
-    
+
     # base dir is aq_git user home folder, need to exist prior to app launch
     # /home/aq_git
     base_dir = root_dir + Settings.application.repo_user
-    
+
     # git_dir is where the repositories are gonna be stored, creating if needed
     # /home/aq_git/git
     git_dir = base_dir + Settings.application.repo_git_path
     git_dir.mkdir if base_dir.exist? && !git_dir.exist?
-    
+
     # repo dir is the repository own path
     # /home/aq_git/git/username
-    repo_dir = git_dir + current_user.name
+    repo_dir = git_dir + current_user.login
     repo_dir.mkdir if !repo_dir.exist?
-    
+
     # the dot git dir is the .git located in the repository
     # /home/aq_git/git/username/reposit.git
     dot_git = repo_dir + (self.name + ".git")
     dot_git.mkdir if !dot_git.exist?
-    
-    self.path = repo_dir.to_s
+
+    self.path = dot_git.to_s
   end
-  
+
   def repo_init
     dirs = { "hooks" => nil,
       "info" => nil,
       "objects" => {"info" => nil, "pack" => nil},
       "refs" => {"heads"=> nil, "tags" => nil}}
     files = ["HEAD", "config", "description"]
-    
+
     dot_git = Pathname(self.path)
 
     # creating the dirs
     l_mkdirs(dot_git, dirs)
-    
+
     # creating base files
     bare = true
     files.each do |l_file|
@@ -53,7 +59,7 @@ class AqRepository < ActiveRecord::Base
         IO.foreach("#{Rails.root}/config/#{template_dir}/#{l_file}") { |w| file_out.puts(w) }
       end
     end
-    
+
   end
 
   # l_mkdirs method creates dirs using a hash
