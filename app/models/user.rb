dfcaa8bdd5c8c20275f863530379c22f9f5fcbfc
T Riboulet <riboulet@gmail.com>
2010-02-24 14:44:37 +0100

    improving the bean view in the repo show

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,13 @@ class User < ActiveRecord::Base
   has_many :rights
   has_many :aq_repositories, :through => :rights
   has_many :beans
+  has_many :beaned_repositories, :through => :beans, :source => :aq_repository
 
   def self.find_by_login_or_email(login)
     find_by_login(login) || find_by_email(login)
+  end
+
+  def max_beans?
+    return true if self.beans.size == 10
   end
 end
