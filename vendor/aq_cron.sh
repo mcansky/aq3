dc97d346dc99d205150f29d422d401fcb1444ccd
T Riboulet <riboulet@gmail.com>
2010-02-25 13:59:01 +0100

    fixing perm problem

--- vendor/aq_cron.sh
+++ vendor/aq_cron.sh
@@ -3,4 +3,5 @@
 cd /Users/mcansky/Code/aquarii/IIIaquarii/
 rake sshkeys:export
 mv /tmp/authorized_keys /Users/aq_git/.ssh
+chown aq_git /Users/aq_git/.ssh/authorized_keys
 chmod 600 /Users/aq_git/.ssh/authorized_keys
