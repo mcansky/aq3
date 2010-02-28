c441152092b00d537d469c9206570f929e6b491b
T Riboulet <riboulet@gmail.com>
2010-02-28 22:17:20 +0100

    improving display list of repositories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
   	@current_user = @current_user_session && @current_user_session.record
   	return @current_user
   end
+
+  def is_even?(a)
+    return true if (a%2 == 0)
+    return false
+  end
 end
