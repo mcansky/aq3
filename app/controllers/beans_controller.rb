1ef4462df7ad0c062b79e309c16c1ee1316b3af5
T Riboulet <riboulet@gmail.com>
2010-04-23 14:49:05 +0200

    removing beans related stuff

--- app/controllers/beans_controller.rb
+++
@@ -1,9 +0,0 @@
-class BeansController < ApplicationController
-  def new
-    a_bean = Bean.new
-    a_bean.aq_repository = AqRepository.find(params[:repository_id])
-    a_bean.user = current_user
-    a_bean.save
-    redirect_to a_bean.aq_repository
-  end
-end
--- app/models/bean.rb
+++
@@ -1,4 +0,0 @@
-class Bean < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :aq_repository
-end
